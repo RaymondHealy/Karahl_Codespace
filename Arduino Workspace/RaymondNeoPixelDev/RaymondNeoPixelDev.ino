2a69d401d018c8b085184fd9409eddc8db92708b
Raymond Healy <Yognaught2@gmail.com>
2017-03-09 18:26:08 -0500

    Did more work on neo pixel libraries

--- Arduino Workspace/RaymondNeoPixelDev/RaymondNeoPixelDev.ino
+++ Arduino Workspace/RaymondNeoPixelDev/RaymondNeoPixelDev.ino
@@ -4,25 +4,41 @@
 
 //Enums-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 enum ColorMode {
-  kNone, kRed, kGreen, kBlue, kWhite, kRainbow, kOldCycle
+  kNone, kRed, kGreen, kBlue, kWhite, kRainbow, kOldCycle, kYellow, kQuasics, kBrown
+};
+
+enum BrightnessMode {
+  kOff, kOn, kBreathing, kBlinking, kDashed, kRollIn, kRollOut, kRolling
 };
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Functions---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 void SetRangeRGB (uint32_t startPixel, uint32_t endPixel, uint32_t red, uint32_t green, uint32_t blue) ;
 void SetRangeHSV(uint32_t startPixel, uint32_t endPixel, float hue, float saturation, float value);
-void ColorModeProcess (ColorMode color);
+void NeoPixelProcess ();
+void SetColorMode (ColorMode color);
+void SetBrightnessMode (BrightnessMode brightness);
+void CycleBrightnessData (uint32_t numberOfSpaces);
+void SetRangeBrightness(uint32_t first, uint32_t last, float brightnessLevel);
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Variables---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 uint32_t modeIteration;
+float * brightnessData;
 
-const float deltaHueForCycle = .5;
+ColorMode colorMode = kRainbow;
+BrightnessMode brightnessMode = kOn;
+
+const float deltaHueForCycle = ;
+const unsigned char brightness = 128;
+const float loopSeconds = fabs(1);
+const uint32_t delayMS = 0;
+
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 //Objects-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-const uint32_t stripLength = 15;
+const uint32_t stripLength = 24;
 const uint32_t pin = 6;
 const uint32_t lastPixel = stripLength - 1;
 Adafruit_NeoPixel* strip;
@@ -31,25 +47,29 @@ RGBConverter rgbConverter;
 
 void setup() {
   strip = new Adafruit_NeoPixel(stripLength, pin, NEO_GRB + NEO_KHZ800);
+
   modeIteration = 0;
 
-  strip->begin();
-  strip->show(); // Initialize all pixels to 'off'
+  brightnessData = new float [strip->numPixels()];
+  for (uint32_t i = 0; i < lastPixel; i++) {
+    brightnessData [i] = 1;
+  }
 
+  strip->setBrightness(brightness);
+  strip->begin();
+  strip->show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  ColorModeProcess(kGreen);
-  strip->show();
-  modeIteration++;
+  NeoPixelProcess();
+  delay(delayMS);
 }
 
 
 //Functions defined
 void SetRangeRGB (uint32_t startPixel, uint32_t endPixel, uint32_t red, uint32_t green, uint32_t blue) {
   for (uint32_t pixel = startPixel; pixel <= endPixel; pixel++) {
-    strip->setPixelColor (pixel, red, green, blue);
+    strip->setPixelColor (pixel, int(float(red)* brightnessData[pixel]), int(float(green)* brightnessData[pixel]), int(float(blue)* brightnessData[pixel]));
   }
 }
 
@@ -61,8 +81,131 @@ void SetRangeHSV(uint32_t startPixel, uint32_t endPixel, float hue, float satura
   SetRangeRGB(startPixel, endPixel, rgb[0], rgb[1], rgb[2]);
 }
 
-void ColorModeProcess (ColorMode color) {
-  switch (color) {
+void NeoPixelProcess () {
+  switch (brightnessMode) {
+    //kDashed
+    case kBlinking: {
+        if (millis() % int(loopSeconds * 1000 + .5) < (loopSeconds * 1000 + .5) / 2) {
+          SetRangeBrightness(0, lastPixel, 0);
+        } else {
+          SetRangeBrightness(0, lastPixel, 1);
+        }
+      }
+      break;
+    case kRollIn: {
+        static uint32_t pixelsFilled = 1;
+        static uint32_t iteration = 0;
+        SetRangeBrightness(0, strip->numPixels() - pixelsFilled, 0);
+        SetRangeBrightness(iteration, iteration, 1);
+
+        if (iteration >= strip->numPixels() - pixelsFilled) {
+          iteration = 0;
+          if (pixelsFilled >= strip->numPixels()) {
+            brightnessMode = kOn;
+            pixelsFilled = 1;
+          } else {
+            pixelsFilled++;
+          }
+        } else {
+          iteration++;
+        }
+      }
+      break;
+    case kRollOut: {
+        static uint32_t pixelsFilled = 1;
+        static uint32_t iteration = 0;
+        SetRangeBrightness(0, strip->numPixels() - pixelsFilled, 1);
+        SetRangeBrightness(iteration, iteration, 0);
+
+        if (iteration >= strip->numPixels() - pixelsFilled) {
+          iteration = 0;
+          if (pixelsFilled >= strip->numPixels()) {
+            brightnessMode = kOff;
+            pixelsFilled = 1;
+          } else {
+            pixelsFilled++;
+          }
+        } else {
+          iteration++;
+        }
+        break;
+      }
+      break;
+    case kRolling: {
+        static uint32_t stage = 0;
+        if (stage == 0) {
+          static uint32_t pixelsFilled = 1;
+          static uint32_t iteration = 0;
+          SetRangeBrightness(0, strip->numPixels() - pixelsFilled, 0);
+          SetRangeBrightness(iteration, iteration, 1);
+
+          if (iteration >= strip->numPixels() - pixelsFilled) {
+            iteration = 0;
+            if (pixelsFilled >= strip->numPixels()) {
+              stage = 1;
+              pixelsFilled = 1;
+            } else {
+              pixelsFilled++;
+            }
+          } else {
+            iteration++;
+          }
+        } else if (stage == 1) {
+          static uint32_t pixelsFilled = 1;
+          static uint32_t iteration = 0;
+          SetRangeBrightness(0, strip->numPixels() - pixelsFilled, 1);
+          SetRangeBrightness(iteration, iteration, 0);
+
+          if (iteration >= strip->numPixels() - pixelsFilled) {
+            iteration = 0;
+            if (pixelsFilled >= strip->numPixels()) {
+              stage = 0;
+              pixelsFilled = 1;
+            } else {
+              pixelsFilled++;
+            }
+          } else {
+            iteration++;
+          }
+        }
+        break;
+      }
+      break;
+    case kBreathing:
+      {
+        const float loopDurration = loopSeconds;
+        const float halfDurration = loopDurration / 2;
+
+        float timeLoop = float(millis() % int(loopDurration * 1000)) / 1000;
+        float timeHalf = float(millis() % int(halfDurration * 1000)) / 1000;
+
+        if (timeLoop <= halfDurration)
+          SetRangeBrightness( 0, lastPixel, (timeHalf * timeHalf) / (halfDurration * halfDurration));
+        else
+          SetRangeBrightness( 0, lastPixel, ((halfDurration * halfDurration) - (timeHalf * timeHalf)) /
+                        (halfDurration * halfDurration));
+      }
+      break;
+    case kOff: {
+        SetRangeBrightness (0, lastPixel, 0);
+      }
+      break;
+    case kOn:
+    default: {
+        SetRangeBrightness (0, lastPixel, 1);
+      }
+      break;
+  }
+  switch (colorMode) {
+    case kBrown:
+        SetRangeRGB(0, lastPixel, 93, 21, 0);
+        break;
+    case kQuasics:
+        SetRangeHSV(0, lastPixel, 120, 1, .62);
+        break;
+    case kYellow:
+        SetRangeRGB(0, lastPixel, 255, 255, 0);
+        break;
     case kRed: {
         SetRangeRGB(0, lastPixel, 255, 0, 0);
       }
@@ -80,14 +223,15 @@ void ColorModeProcess (ColorMode color) {
       }
       break;
     case kRainbow: {
-        float delta = 360 / strip->numPixels();
         float baseHue = modeIteration * deltaHueForCycle;
 
+        while (baseHue < 0){
+          baseHue = 360 - fabs(baseHue);
+        }
+
+        
         for (uint32_t pixel = 0; pixel <= lastPixel; pixel++) {
-          if (baseHue + deltaHueForCycle * pixel <= 360)
-            SetRangeHSV(pixel, pixel, baseHue + deltaHueForCycle * pixel, 1, 1);
-          else
-            SetRangeHSV(pixel, pixel, deltaHueForCycle * pixel, 1, 1);
+          SetRangeHSV(pixel, pixel, baseHue +  pixel * 360 / strip->numPixels(), 1, 1);
         }
         modeIteration = modeIteration % int(360 / deltaHueForCycle);
       }
@@ -102,4 +246,24 @@ void ColorModeProcess (ColorMode color) {
       }
       break;
   }
+
+  modeIteration++;
+  strip->show();
+}
+
+void SetColorMode (ColorMode color) {
+  colorMode = color;
+}
+
+void CycleBrightnessData (uint32_t numberOfSpaces){
+  
+}
+
+void SetBrightnessMode (BrightnessMode brightness) {
+  brightnessMode = brightness;
+}
+
+void SetRangeBrightness(uint32_t first, uint32_t last, float brightnessLevel) {
+  for (uint32_t pixel = first; pixel <= last; pixel++)
+    brightnessData[pixel] = brightnessLevel;
 }
