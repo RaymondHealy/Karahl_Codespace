d2569cabb8e09d73e516fa9b9c7100184b59c153
Raymond Healy <Yognaught2@gmail.com>
2017-07-28 17:54:48 -0400

    RobotLighting Fix

--- Arduino Workspace/RobotLighting/RobotLighting.ino
+++ Arduino Workspace/RobotLighting/RobotLighting.ino
@@ -1,22 +1,16 @@
 #include <NeoPixelController.h>
-#define isRed
 
-
-const uint32_t stripLength = 36;
+const uint32_t stripLength = 24;
 const uint32_t pin = 6;
-const uint8_t brightness = 255;
-const float loopLength = 1;
+const uint8_t brightness = 64;
+const float loopLength = 1.5;
 NeoPixelController* strip;
 
 
 void setup() {
   // put your setup code here, to run once:
   strip = new NeoPixelController(pin, loopLength, brightness, stripLength);
-#ifdef isRed
-  strip->SetColorMode(NeoPixelController::kRed);
-#else
-  strip->SetColorMode(NeoPixelController::kBlue);
-#endif
+  strip->SetColorMode(NeoPixelController::kPurple);
   strip->SetBrightnessMode (NeoPixelController::kOn);
   strip->SetPixelsPerSegment(6);
 }
